c986c3252105a17785eaaffa6e45fe25a70a34f3
chinmaykrishnroy <morpheusprefecttt@gmail.com>
2024-10-07 14:49:13 +0530

    added scripts

--- run.bat
+++ run.bat
@@ -1,2 +1,30 @@
-python -m pip install PyQt5==5.15.10 git+https://github.com/CPJKU/madmom
+@echo off
+setlocal
+
+:: Get the absolute path of the current directory
+set "PROJECT_DIR=%~dp0"
+cd /d "%PROJECT_DIR%"
+
+:: Set the virtual environment directory
+set "VENV_DIR=%PROJECT_DIR%venv"
+
+:: Check if the virtual environment directory exists
+if not exist "%VENV_DIR%\Scripts\python.exe" (
+    echo Creating virtual environment...
+    python -m venv "%VENV_DIR%"
+)
+
+:: Activate the virtual environment
+call "%VENV_DIR%\Scripts\activate.bat"
+
+:: Install the required packages
+python -m pip install -r requirements.txt
+cls
+
+:: Run the main Python script
 python main.py
+
+:: Deactivate the virtual environment
+deactivate
+
+endlocal
