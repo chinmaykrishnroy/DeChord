18c78ce87c8ce457de611dff484fe28ff8fde2e8
Chinmay Krishn Roy <morpheusprefecttt@gmail.com>
2024-11-22 16:35:50 +0530

    Improved Key Signature Accuracy and added Caching

--- key.py
+++ key.py
@@ -1,11 +1,7 @@
-from PyQt5.QtCore import QThread, pyqtSignal, QMutex
+import os
+import hashlib
+from PyQt5.QtCore import QThread, pyqtSignal
 import madmom
-
-key_mapping = [
-    "C / Am", "C# / A#m", "D / Bm", "D# / Cm",
-    "E / C#m", "F / Dm", "F# / D#m", "G / Em",
-    "G# / Fm", "A / F#m", "A# / Gm", "B /G#m"
-]
 
 class KeyRecognitionThread(QThread):
     result = pyqtSignal(str)
@@ -13,15 +9,28 @@ class KeyRecognitionThread(QThread):
     def __init__(self, audio_path):
         super().__init__()
         self.audio_path = audio_path
-        self.mutex = QMutex()
 
     def run(self):
-        key_processor = madmom.features.key.CNNKeyRecognitionProcessor()
-        key = key_processor(self.audio_path)
-        most_likely_key_index = key.argmax()
-        most_likely_key = key_mapping[most_likely_key_index]
-        print(f"Key: {most_likely_key}")
-        self.mutex.lock()
-        self.result.emit(most_likely_key)
-        self.mutex.unlock()
+        cache_dir = "cache/key/"
+        os.makedirs(cache_dir, exist_ok=True)
+        hash_object = hashlib.md5(self.audio_path.encode())
+        hashed_filename = hash_object.hexdigest() + ".txt"
+        cache_file = os.path.join(cache_dir, hashed_filename)
+
+        if os.path.exists(cache_file):
+            with open(cache_file, "r") as f:
+                cached_key = f.read().strip()
+            self.result.emit(cached_key)
+            self.quit()
+            return
+
+        try:
+            key_processor = madmom.features.key.CNNKeyRecognitionProcessor()
+            key_prediction = key_processor(self.audio_path)
+            key = madmom.features.key.key_prediction_to_label(key_prediction)
+            with open(cache_file, "w") as f:
+                f.write(key)
+            self.result.emit(key)
+        except Exception as e:
+            self.result.emit("Error")
         self.quit()
